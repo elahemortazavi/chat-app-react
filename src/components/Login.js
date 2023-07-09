fc606204b59015873ddd6e0750513db04cca983e
elahemortazavi <emor5680@gmail.com>
2023-07-09 23:56:21 +0100

    login.module.css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import google from "../assets/google.svg";
+import styles from "./Login.module.css";
 
 const Login = () => {
     return (
-        <div>
-            <div>
-                <h2>Welcome to Elahe's Chat App</h2>
-                <div>
-                    <img src={google} alt="google-icon" />Sign in with google
-                </div>
-            </div>
-
+      <div className={styles.loginPage}>
+        <div className={styles.loginCard}>
+          <h2>Welcome to Elahe's Chat App</h2>
+          <div className={styles.button}>
+            <img src={google} alt="google-icon" />
+            Sign in with google
+          </div>
         </div>
+      </div>
     );
 };
 
